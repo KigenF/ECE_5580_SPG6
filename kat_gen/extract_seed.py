edbb1419b4c70621ffb28210a326479835aed5df
福田岐弦 <kigen@kigenmacbook.mynetworksettings.com>
2025-03-16 14:37:12 -0400

    KAT generation modified

--- kat_gen/extract_seed.py
+++ kat_gen/extract_seed.py
@@ -3,17 +3,17 @@ def extract_seeds(input_file, output_file):
     with open(input_file, "r") as fin:
         for line in fin:
             line = line.strip()
-            # "seed =" で始まる行を検出
+            # detect line "seed =" 
             if line.startswith("seed ="):
-                # "=" 以降の部分を抽出して、空白を除去
+                # extract after "=" 
                 _, seed_value = line.split("=", 1)
                 seeds.append(seed_value.strip())
     
-    # 抽出した seed を1行ずつ出力
+    # output seed
     with open(output_file, "w") as fout:
         for seed in seeds:
             fout.write(seed + "\n")
 
 
 if __name__ == "__main__":
-    extract_seeds("PQCkemKAT_16.req", "PQCkemKAT_16-seed.txt")
+    extract_seeds("Original_PQCkemKAT_16.req", "PQCkemKAT_16-seed.txt")
