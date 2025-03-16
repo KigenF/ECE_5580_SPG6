33a9abb61f07ef10a5a36ba44888b308e24a5cca
Pttuser-56qq <hhh555jjj1@gmail.com>
2025-03-16 04:41:02 -0400

    Initial commit

---
+++ r5_cpa_kem_core.c
@@ -0,0 +1,193 @@
+#include "r5_cpa_kem_keygen.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <libkeccak.a.headers/SP800-185.h>
+
+#define len_tau_2 2048
+#define d 594
+#define n 1
+#define k 594 // k = d/n.
+#define kappa_bytes 16
+#define q 8192
+
+#define SHAKE128_RATE 168
+#define SHAKE256_RATE 136
+
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#define LITTLE_ENDIAN16(x) (x)
+#else
+#define LITTLE_ENDIAN16(x) ( \
+    (((x) & 0xFF00U) >> 8) |  \
+    (((x) & 0x00FFU) << 8)    \
+)
+#endif
+
+typedef cSHAKE_Instance cshake_ctx;
+
+typedef struct {
+    cshake_ctx generator_ctx; 
+    uint8_t output[SHAKE128_RATE > SHAKE256_RATE ? SHAKE128_RATE : SHAKE256_RATE]; 
+    size_t index;
+    void (* generate)(uint8_t *output, const size_t output_len);
+} drbg_ctx;
+
+static drbg_ctx ctx;
+static const uint8_t permutation_customization[2] = {0, 1};
+
+void print_hex(const char *var, const unsigned char *data, const size_t nr_elements, const size_t element_size) {
+    size_t i, ii;
+    if (var != NULL) {
+        printf("%s[%zu]=", var, nr_elements);
+    }
+    for (i = 0; i < nr_elements; ++i) {
+        if (i > 0) {
+            printf(" ");
+        }
+        for (ii = element_size; ii > 0; --ii) {
+            printf("%02hhX", data[i * element_size + ii - 1]);
+        }
+    }
+    if (var != NULL) {
+        printf("\n");
+    }
+}
+
+void *checked_malloc(size_t size) {
+	void *tmp = malloc(size);
+	if(tmp == NULL) abort();
+	return tmp;
+}
+
+void *checked_calloc(size_t count, size_t size) {
+	void *temp = calloc(count, size);
+	if (temp == NULL) {
+		abort();
+	}
+	return temp;
+}
+
+void cshake128(unsigned char *output, size_t output_len, const unsigned char *input, const size_t input_len, const unsigned char *customization, const size_t customization_len) {
+	if (cSHAKE128(input, input_len * 8, output, output_len * 8, NULL, 0, customization, customization_len) != 0) {
+		abort();
+	}
+}
+
+int drbg_sampler16_2_once(uint16_t *x, const size_t xlen, const void *seed, const size_t seed_size, const uint32_t range) {
+	cshake128((uint8_t *) x, xlen * sizeof (uint16_t), (const uint8_t *) seed, seed_size, NULL, 0); // For the case where seed is 16 bytes.
+
+    /* Bring in range and flip byte order if necessary */
+    for (size_t i = 0; i < xlen; ++i) {
+        x[i] = (uint16_t) ((LITTLE_ENDIAN16(x[i])) & (range - 1));
+    }
+
+    return 0;
+}
+
+void create_A_master(uint8_t * in, uint16_t * out) {
+	drbg_sampler16_2_once(out, len_tau_2, in, kappa_bytes, q);
+}
+
+void cshake128_init(cshake_ctx *ctx, const unsigned char *customization, const size_t customization_len) {
+	if (cSHAKE128_Initialize(ctx, 0, NULL, 0, customization, customization_len * 8) != 0) {
+		abort();
+	}
+}
+
+void cshake128_absorb(cshake_ctx *ctx, const unsigned char *input, const size_t input_len) {
+	if (cSHAKE128_Update(ctx, input, input_len * 8) != 0) {
+		abort();
+	}
+	if (cSHAKE128_Final(ctx, NULL) != 0) {
+		abort();
+	}
+}
+
+void cshake128_squeezeblocks(cshake_ctx *ctx, unsigned char *output, const size_t nr_blocks) {
+	if (cSHAKE128_Squeeze(ctx, output, nr_blocks * SHAKE128_RATE * 8) != 0) {
+		abort();
+	}
+}
+
+static void generate_cshake128(uint8_t *output, const size_t output_len) {
+    size_t i, j;
+
+    i = ctx.index;
+    for (j = 0; j < output_len; j++) {
+        if (i >= SHAKE128_RATE) {
+            cshake128_squeezeblocks(&ctx.generator_ctx, ctx.output, 1);
+            i = 0;
+        }
+        output[j] = ctx.output[i++];
+    }
+    ctx.index = i;
+}
+
+void drbg_init_customization(const void *seed, const size_t seed_size, const uint8_t *customization, const size_t customization_len) {
+	cshake128_init(&ctx.generator_ctx, customization, customization_len);
+	cshake128_absorb(&ctx.generator_ctx, seed, seed_size);
+	ctx.index = SHAKE128_RATE;
+	ctx.generate = &generate_cshake128;
+}
+
+int drbg(void *x, const size_t xlen) {
+    ctx.generate(x, xlen);
+    return 0;
+}
+
+uint16_t drbg_sampler16_2(const uint32_t range) {
+    uint16_t rnd;
+    drbg(&rnd, sizeof (rnd));
+    rnd = LITTLE_ENDIAN16(rnd);
+    rnd = (uint16_t) (rnd & (range - 1));
+    return rnd;
+}
+
+static int permutation_tau_2(uint32_t *row_disp, const unsigned char *seed) {
+    uint32_t i;
+    uint16_t rnd;
+    uint8_t *v = checked_calloc(len_tau_2, 1);
+
+    drbg_init_customization(seed, kappa_bytes, permutation_customization, sizeof (permutation_customization));
+
+    for (i = 0; i < k; ++i) {
+        do {
+            rnd = drbg_sampler16_2(len_tau_2);
+        } while (v[rnd]);
+        v[rnd] = 1;
+        row_disp[i] = rnd;
+    }
+
+    free(v);
+
+    return 0;
+}
+
+void create_A(uint8_t *sigma, uint16_t *A) {
+    uint32_t i;
+    uint16_t *A_master = NULL;
+    uint32_t *A_permutation = NULL;
+    const uint16_t els_row = (uint16_t) (k * n);
+    size_t len_a;
+
+    len_a = (size_t) (k * k * n);
+
+    /* Create A/A_Master*/
+    //A = checked_malloc(len_a * sizeof (*A));
+	A_master = checked_malloc((size_t) (len_tau_2 + d) * sizeof (*A_master));
+	create_A_master(sigma, A_master);
+	memcpy(A_master + len_tau_2, A_master, d * sizeof (*A_master));
+
+    /* Compute and apply the permutation to get A */
+	A_permutation = checked_malloc(k * sizeof (*A_permutation));
+
+	/* Compute and apply permutation */
+	permutation_tau_2(A_permutation, sigma);
+	for (i = 0; i < k; ++i) {
+		memcpy(A + (i * els_row), A_master + A_permutation[i], els_row * sizeof (*A));
+	}
+
+	/* Free allocated memory */
+	free(A_master);
+	free(A_permutation);
+}
