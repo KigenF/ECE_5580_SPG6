1a77fd2a3fed4dd4034713d456660b063a75d032
trkibler <tylerryan1128@gmail.com>
2025-03-17 01:10:39 -0400

    KAT Tester For KeyGen
    Initial Version of KAT tester

---
+++ kat_gen/kat_tester.py
@@ -0,0 +1,40 @@
+# def check(inputa, inputb):
+
+def list_to_string(listto:list) -> str:
+    string= ""
+    for row in listto:
+        string += row.replace("\n", "")
+    return string
+
+pk_list = []
+with open("PQCkemKAT_16_cryptolReady.rsp", "r") as file:
+    save_next = False
+    for lines in file:
+        if save_next:
+            pk_list.append(lines.strip())
+            save_next = False
+        elif lines.strip() == 'pk':
+            save_next = True
+    print(len(pk_list))
+
+compare_list = []
+sublist = []
+with open("message-full.txt", "r") as file:
+    save_next = False
+    for lines in file:
+        sublist.append(lines)
+        if lines.strip().find("]") >= 0:
+            compare_list.append(list_to_string(sublist))
+            sublist = []
+    print(len(compare_list))
+        
+successes = 0
+for j in zip(pk_list, compare_list):
+    # in_line = input(">")
+    in_line = str(j[1])
+    in_line = in_line.replace(" ", "").upper()
+    original = str(j[0]).upper()
+    if in_line.strip() == original.strip():
+        successes+=1
+print(f"{successes}/100")
+    
