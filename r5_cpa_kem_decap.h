420bb46d6a73bbf6faae2679b69972d9006eefc8
Pttuser-56qq <hhh555jjj1@gmail.com>
2025-03-24 12:31:29 -0400

    Add externals.

---
+++ r5_cpa_kem_decap.h
@@ -0,0 +1,5 @@
+#include "r5_cpa_kem_common.h"
+void unpack_ct(uint8_t * in, uint16_t * out_0, uint8_t * out_1);
+void decompress_matrix(uint8_t * in, uint16_t * out);
+void _pack(uint8_t * in, uint8_t * out);
+void hash(uint8_t * in, uint8_t * out);
